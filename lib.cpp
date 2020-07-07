bd5104cb6b2ab0fc08688e056a90f5cd1c5a784b
Alex Scherba <alexscherba16@gmail.com>
2020-07-07 13:14:09 +0300

    add lib

---
+++ lib.cpp
@@ -0,0 +1,7 @@
+#include "lib.h"
+
+
+int version()
+{
+    return 133742;
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
+#include "lib.h"
+
 #include <iostream>
 
 int main (int, char **)
 {
-    std::cout << "Hello Otus homework_0 " << std::endl;
+    std::cout << "Hello Otus homework_0 " << std::endl 
+    << "Current version: " << version() << std::endl;
     return 0;
 }
